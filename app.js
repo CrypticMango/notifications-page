82885b479033ebc5fb80bcaaa750433667144d1b
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-11-11 10:42:41 -0600

    completed function for mark as read button

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const readButton = document.getElementById("read-btn");
 const notifNumber = document.getElementById("three");
-console.log(readButton);
+const redDot = document.getElementsByClassName("red-dot");
+const newNotifBackground = document.getElementsByClassName("notification-card-b");
 
 readButton.addEventListener("click", markAsRead);
 
 function markAsRead() {
     notifNumber.innerHTML = 0;
+    redDot[0].style.display = "none";
+    redDot[1].style.display = "none";
+    redDot[2].style.display = "none";
+    newNotifBackground[0].style.background = "none";
+    newNotifBackground[1].style.background = "none";
+    newNotifBackground[2].style.background = "none";
 }
